0fa5eca10e84b12e8e497f4b9021c7af0352d6a0
HenryHirsch <122999149+henhirsch@users.noreply.github.com>
2023-07-06 12:26:23 -0400

    minor change

--- econ_sig_rules.R
+++ econ_sig_rules.R
@@ -9,6 +9,7 @@ library(dplyr)
 library(extrafont)
 library(grid)
 library(ggnewscale)
+library(stringr)
 
 # get custom font
 # font_import(path='/Users/henryhirsch/Henry/Work/2023/Regulatory Studies Center/projects/project 2 (regstats graphs)/GW Logos and Fonts/GW Fonts')
@@ -29,13 +30,14 @@ sig$excluding.withdrawn <- NULL
 # remove rows with NA values
 sig <- sig[complete.cases(sig), ]
 
-# create party column (this must be manually updated with years of Democrat Presidents)
-sig$party <- ifelse(sig$year %in% c(1993:2000, 2009:2016, 2021:2024), "dem", "rep")
+# create party column ("demyears" must be manually updated with years of Democrat Presidents)
+demyears <- c(1993:2000, 2009:2016, 2021:2024)
+sig$party <- ifelse(sig$year %in% demyears, "dem", "rep")
 
 # make party factor variable
 sig$party <- as.factor(sig$party)
 
-# Generate bar1
+# generate bar1
 bar1 <- ggplot(sig, aes(x = year, y = econ, fill = party)) +
   geom_bar(stat = "identity", width = 0.5) +
   scale_fill_manual(values = c("blue", "red")) +
@@ -55,11 +57,11 @@ bar1 <- ggplot(sig, aes(x = year, y = econ, fill = party)) +
 
 bar1
 
-# Generate bar2
+# generate bar2
 y_lines <- c(0, 25, 50, 75, 100, 125, 150)
 
 num_lines <- 4
-increment <- max(150) / (num_lines + 1)
+increment <- 150 / (num_lines + 1)
 additional_lines <- seq(increment, max(sig$econ), by = increment)
 
 bar2 <- bar1 +
@@ -73,16 +75,45 @@ bar2 <- bar1 +
 
 bar2
 
-# Generate bar3
+# generate bar3
 current_date <- format(Sys.Date(), "%B %d, %Y")
 
-caption_text <- paste("Sources: Office of the Federal Register (federalregister.gov) for the years starting 2021;\nOffice of Information and Regulatory Affairs (OIRA) (reginfo.gov) for all prior years.\n\nUpdated:", current_date)
+caption_text <- paste("Sources: Office of the Federal Register (federalregister.gov) for the years starting 2021;\n       Office of Information and Regulatory Affairs (OIRA) (reginfo.gov) for all prior years.\n\nUpdated:", current_date)
 
 bar3 <- bar2 +
   labs(caption = caption_text) +
-  theme(plot.caption = element_text(hjust = 1, margin = margin(t = 0, unit = "pt")))
+  theme(plot.caption = element_text(hjust = 1, margin = margin(t = 0, l = 6, unit = "pt")))
 
 bar3
+
+# # Generate caption text
+# current_date <- format(Sys.Date(), "%B %d, %Y")
+# 
+# caption_text <- paste("Sources: Office of the Federal Register (federalregister.gov) for the years starting 2021; Office of Information and Regulatory Affairs (OIRA) (reginfo.gov) for all prior years.>",
+#                       "Updated:", current_date)
+# 
+# # Add line breaks to align sentences
+# caption_text_wrapped <- str_wrap(caption_text, width = 50)
+# 
+# # Create a function to format the caption
+# format_caption <- function(caption) {
+#   lines <- str_split(caption, pattern = ">")[[1]]
+#   formatted_lines <- paste0("\n", lines, "\n")
+#   formatted_caption <- paste0("\n\n", paste(formatted_lines, collapse = ""), "\n")
+#   return(formatted_caption)
+# }
+# 
+# # Format the caption
+# formatted_caption <- format_caption(caption_text_wrapped)
+# 
+# # Create the plot with the formatted caption
+# bar3 <- bar2 +
+#   labs(caption = formatted_caption) +
+#   theme(plot.caption = element_text(hjust = 1, margin = margin(t = 0, unit = "pt")))
+# 
+# bar3
+
+
 
 # add tick marks
  bar3 +
