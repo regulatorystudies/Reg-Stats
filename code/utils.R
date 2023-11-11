e9ce2908c4731228633980d50cca399c2bb705fb
Mark Febrizio <63374265+mfebrizio@users.noreply.github.com>
2023-11-10 20:38:11 -0500

    remove moved files

--- code/utils.R
+++
@@ -1,7 +0,0 @@
-#library(grid)
-#library(png)
-
-# loads png file
-get_png <- function(filename) {
-  grid::rasterGrob(png::readPNG(filename), interpolate = TRUE) # adjust logo size dimensions here
-}
