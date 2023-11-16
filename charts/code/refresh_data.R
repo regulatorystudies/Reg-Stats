f8d1f3ea6867e3b2a4d0212631a088d6c3ee0be1
Mark Febrizio <63374265+mfebrizio@users.noreply.github.com>
2023-11-15 19:38:23 -0500

    Update refresh_data.R

--- charts/code/refresh_data.R
+++ charts/code/refresh_data.R
@@ -1,29 +1,25 @@
+# import libraries
 library(here)
 library(fs)
 
+source(here("charts", "code", "local_utils.R"))
+
+# set file location relative to root
 i_am("charts/code/refresh_data.R")
 
-copy_data <- function(path, new_path){
-  
-  file_list <- dir_ls(path, recurse = T, type = "file", glob = "*.csv")
-  file_copy(file_list, new_path, overwrite = T)
-  return(paste("Copied", length(file_list), "files"))
-  
-}
-
+# establish location for storing charts data
 path_chartdata <- path(here("charts", "data"))
-
 if (!dir_exists(path_chartdata)) {
   
   dir_create(path_chartdata)
   
 }
 
+# establish location for data subfolder
 path_data <- path(here("data"))
-
 if (dir_exists(path_data)) {
   
-  copy_data(path_data, path_chartdata)
+  copy_all_data(path_data, path_chartdata)
   
 } else {
   
