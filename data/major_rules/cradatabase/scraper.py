1636ca2245c880f41171f8f197db6c572a84f230
Mark Febrizio <63374265+mfebrizio@users.noreply.github.com>
2023-11-30 22:48:18 -0500

    Update scraper.py

--- data/major_rules/cradatabase/scraper.py
+++ data/major_rules/cradatabase/scraper.py
@@ -8,29 +8,33 @@ import requests
 from bs4 import BeautifulSoup
 
 
-URL_STEM = "https://www.gao.gov"
-BASE_URL = "https://www.gao.gov/legal/other-legal-work/congressional-review-act"
-BASE_PARAMS = {
-    "processed": 1, 
-    "type": "all", 
-    "priority": "all", 
-    "page": 0
-    }
-
-
 class ParseError(Exception):
     pass
 
 
 class Scraper:
-    
+    """Base scraper class for retrieving data from GAO's Congressional Review Act (CRA) database."""    
     def __init__(
             self, 
-            base_url: str = BASE_URL, 
-            url_stem: str = URL_STEM, 
-            request_params: dict = BASE_PARAMS, 
+            base_url: str = "https://www.gao.gov/legal/other-legal-work/congressional-review-act", 
+            url_stem: str = "https://www.gao.gov", 
+            request_params: dict = {
+                "processed": 1, 
+                "type": "all", 
+                "priority": "all", 
+                "page": 0
+                }, 
             major_only: bool = False, 
             new_only: bool = False, **kwargs):
+        """Initialize base scraper for CRA database.
+
+        Args:
+            base_url (str, optional): URL to pass to `requests.get()`. Defaults to "https://www.gao.gov/legal/other-legal-work/congressional-review-act".
+            url_stem (str, optional): Stem of GAO website URL for constructing rule-level URLs. Defaults to "https://www.gao.gov".
+            request_params (dict, optional): Parameters to pass to `requests.get()`. Defaults to {"processed": 1, "type": "all", "priority": "all", "page": 0}.
+            major_only (bool, optional): Only retrieve rules of type "Major". Defaults to False.
+            new_only (bool, optional): Only retrieve rules added since last collection date (uses "date_retrieved" from existing dataset). Defaults to False.
+        """
         self.url = base_url
         self.url_stem = url_stem
         self.params = request_params
@@ -43,7 +47,16 @@ class Scraper:
             self.params.update({"received_start_date": start_date})
     
     def request_soup(self, page: int = None, alt_url: str = None, parser: str = "lxml"):
-        
+        """Request and parse html using `requests` and `bs4`, returning a processed `BeautifulSoup` object.
+
+        Args:
+            page (int, optional): Page number for updating requests parameters. Defaults to None.
+            alt_url (str, optional): Supply an alternate URL for the request, rather than the default `base_url`. Defaults to None.
+            parser (str, optional): Parser to pass to `BeautifulSoup()`. Defaults to "lxml".
+
+        Returns:
+            BeautifulSoup: Parsed html as a BeautifulSoup object.
+        """        
         if page is not None:  # only updates parameters when page number is given
             self.params.update({"page": page})
         
@@ -60,14 +73,28 @@ class Scraper:
         return soup
     
     def from_json(self, path: Path, file_name: str) -> dict | list:
-        
+        """Import data from .json format.
+
+        Args:
+            path (Path): Path of directory where file is located.
+            file_name (str): Name of .json file (without file extension; e.g., "file_name").
+
+        Returns:
+            dict | list: JSON object.
+        """        
         with open(path / f"{file_name}.json", "r", encoding="utf-8") as f:
             data = json.load(f)
         
         return data
     
     def to_json(self, data: dict | list, path: Path, file_name: str) -> None:
-        
+        """Save retrieved data as .json. Prints save path if successful.
+
+        Args:
+            data (dict | list): Data to save to JSON.
+            path (Path): Path of directory where file should be located.
+            file_name (str): Name of .json file (file extension is added automatically).
+        """        
         with open(path / f"{file_name}.json", "w", encoding="utf-8") as f:
             json.dump(data, f, indent=4)
         
@@ -75,18 +102,36 @@ class Scraper:
 
 
 class PopulationScraper(Scraper):
-    
+    """Scraper for retrieving basic data on the population of rules in GAO's CRA database, including their url, type, and effective date."""    
     def __init__(self, **kwargs):
+        """Initialize PopulationScraper, inheriting atrributes and methods from Scraper class."""        
         super().__init__(**kwargs)
 
     def get_document_count(self, soup: BeautifulSoup):
-        
+        """Retrieve document count returned by the search of the CRA database.
+
+        Args:
+            soup (BeautifulSoup): Parsed html of webpage.
+
+        Returns:
+            int: Number of documents meeting the search parameters.
+        """        
         results_count = soup.find("div", class_="count")
         document_count = int(results_count.text.strip().split()[-1])
         return document_count
         
     def get_page_count(self, soup: BeautifulSoup):
-        
+        """Retrieve page count returned by the search of the CRA database.
+
+        Args:
+            soup (BeautifulSoup): Parsed html of webpage.
+
+        Raises:
+            ParseError: Error parsing the html for the variable of interest.
+
+        Returns:
+            int: Number of pages containing documents returned by the search parameters.
+        """        
         results_pages = soup.find_all("span", class_="usa-pagination__link-text")
         
         if results_pages:
@@ -102,8 +147,15 @@ class PopulationScraper(Scraper):
         
         return page_count
     
-    def scrape_population(self, pages: int, url_stem: str = URL_STEM):
-        
+    def scrape_population(self, pages: int):
+        """Scrape html for population of rules in CRA database.
+
+        Args:
+            pages (int): Number of pages containing rule data. In practice, this should be equivalent to the value returned by `get_page_count()`.
+
+        Returns:
+            dict[str]: Results of the scraped data and accompanying metadata.
+        """
         all_rules = []
         for page in range(pages + 1):  # zero-based numbering
             # get soup for this page
@@ -117,7 +169,7 @@ class PopulationScraper(Scraper):
                 effective_date = rule.find("time")
                 rule_dict = {
                     "page": page, 
-                    "url": f"{url_stem}{bookmark['href']}", 
+                    "url": f"{self.url_stem}{bookmark['href']}", 
                     "type": bookmark.string.strip(), 
                     "effective_date": effective_date["datetime"]
                     }
@@ -129,17 +181,13 @@ class PopulationScraper(Scraper):
                 print(f"Retrieved {len(all_rules)} rules from {page + 1} pages.")
         
         type_counts = Counter([rule["type"] for rule in all_rules])
-        if pages > 1:
-            print(f"Retrieved {type_counts.total()} rules from {page + 1} pages.")
-        elif len(pages) == 1:
-            print(f"Retrieved {type_counts.total()} rules from {len(pages)} page.")
-        
+        print(f"Retrieved {type_counts.total()} rules from {page + 1} pages.")
         for k, v in type_counts.items():
             print(f"{k}s: {v}")
         
         output = {
             "description": "Contains basic records for rules in GAO's CRA Database of Rules.", 
-            "source": BASE_URL, 
+            "source": self.url, 
             "date_retrieved": f"{date.today()}", 
             "major_only": self.major_only, 
             "rule_count": len(all_rules), 
@@ -150,16 +198,28 @@ class PopulationScraper(Scraper):
 
 
 class RuleScraper(Scraper):
-    
+    """Scraper for retrieving detailed data on rules in GAO's CRA database."""
     def __init__(self, input_data: dict | list = None, **kwargs) -> None:
+        """Initialize RuleScraper, inheriting atrributes and methods from Scraper class.
+
+        Args:
+            input_data (dict | list, optional): Data on rules passed from PopulationScraper. Defaults to None.
+        """        
         super().__init__(**kwargs)
         if input_data is None:
             self.data = {}
         else:
             self.data = input_data
     
-    def scrape_rule(self, url: str):  #soup: BeautifulSoup):
-        
+    def scrape_rule(self, url: str):
+        """Scrape detailed data for a single rule.
+
+        Args:
+            url (str): URL to that rule's page (with pattern "https://www.gao.gov/fedrules/100000").
+
+        Returns:
+            dict[str]: Results of the scraped data.
+        """        
         rule_data = {"url": url}
         soup = self.request_soup(alt_url = url)
         page_contents = soup.find("div", class_="main-page-content--inner")
@@ -195,7 +255,15 @@ class RuleScraper(Scraper):
         return rule_data
     
     def scrape_rules(self, path: Path = None, file_name: str = None):
-        
+        """Scrape detailed data for multiple rules.
+
+        Args:
+            path (Path, optional): Path to input data. Defaults to None.
+            file_name (str, optional): File name of input data. Defaults to None.
+
+        Returns:
+            dict[str]: Results of the scraped data and accompanying metadata.
+        """        
         if (path is not None) and (file_name is not None):
             # import json
             self.data = self.from_json(path, file_name)
@@ -211,12 +279,17 @@ class RuleScraper(Scraper):
             #soup = self.request_soup(alt_url = rule.get("url"))
             rule_data = self.scrape_rule(url = rule.get("url"))
             all_rule_data.append(rule_data)
+            
+            print_frequency = (len(rules) // 10) + 1
+            if len(rules) % print_frequency == 0:
+                print(f"Retrieved detailed information for {len(all_rule_data)} rules.")
+        
+        print(f"Retrieved detailed information for {len(all_rule_data)} rules.")
         
         example_control_number = r"{control_number}"
-        
         output = {
             "description": "Contains detailed records for rules in GAO's CRA Database of Rules.", 
-            "sources": f"Series of links with pattern {URL_STEM}/{example_control_number}", 
+            "sources": f"Series of links with pattern {self.url_stem}/fedrules/{example_control_number}", 
             "date_retrieved": f"{date.today()}", 
             "rule_count": len(all_rule_data), 
             "results": all_rule_data
@@ -226,25 +299,52 @@ class RuleScraper(Scraper):
 
 
 def has_field_name(class_):
+    """Helper function to pass to BeautifulSoup `find_all()` method. Identifies html containing field names."""
     return re.compile("field field--name-field-").search(class_)
 
 
 def clean_label(label: str, replace_with: str = "_"):
+    """Clean field labels.
+
+    Args:
+        label (str): Field label.
+        replace_with (str, optional): Character(s) that will replace whitespace. Defaults to "_".
+
+    Returns:
+        str: Cleaned field label.
+    """    
     return re.compile("[\s\.]", re.I).sub(replace_with, label).replace("__", replace_with)
 
 
 def get_retrieval_date(path : Path, file_name: str):
-    
+    """Get `date_retrieved` of existing data from .json file.
+
+    Args:
+        path (Path): Path to file.
+        file_name (str): File name.
+
+    Returns:
+        str: String of retrieval date in YYYY-MM-DD format.
+    """    
     with open(path / f"{file_name}.json", "r", encoding="utf-8") as f:
         data = json.load(f)
     
     return data["date_retrieved"]
     
 
-def main(major_only: bool = False, new_only: bool = False, **kwargs):
-    
-    # initialize scraper
+def main(data_path: Path, major_only: bool = False, new_only: bool = False, **kwargs):
+    """Executes main pipeline for retrieving data from GAO's CRA database.
+
+    Args:
+        major_only (bool, optional): Retrieve only major rules. Defaults to False.
+        new_only (bool, optional): Retrieve only new rules received by GAO (when True, must pass `path` and `file_name` as **kwargs). Defaults to False.
+    """
+    # initialize PopulationScraper
     ps = PopulationScraper(major_only=major_only, new_only=new_only, **kwargs)
+    if major_only:
+        type = "major"
+    else:
+        type = "all"
     
     # request and parse html
     soup = ps.request_soup()
@@ -252,13 +352,17 @@ def main(major_only: bool = False, new_only: bool = False, **kwargs):
     page_count = ps.get_page_count(soup)
     print(f"Requesting {document_count} rules from {page_count + 1} pages.")
     
-    # scrape rules
-    data = ps.scrape_population(page_count)
+    # scrape population data and save
+    pop_data = ps.scrape_population(page_count)
+    ps.to_json(pop_data, data_path, f"population_{type}")
     
-    # save to json
-    file_name = f"population_{ps.params['type']}".lower()
-    ps.to_json(data, data_path, file_name)
+    # initialize RuleScraper
+    rs = RuleScraper(input_data=pop_data)
     
+    # scrape rule detail data and save
+    rule_data = rs.scrape_rules()
+    rs.to_json(rule_data, data_path, f"rule_detail_{type}")
+
 
 if __name__ == "__main__":
     
@@ -271,8 +375,38 @@ if __name__ == "__main__":
     if not data_path.is_dir():
         data_path.mkdir(parents=True, exist_ok=True)
     
-    # call scraper pipeline
-    main(major_only=True, new_only=True, path=data_path, file_name="population_test")
+    while True:
+        
+        # print prompts to console
+        major_prompt = input("Retrieve only major rules? [yes/no]: ").lower()
+        new_prompt = input("Retrieve only new rules (i.e., those received by GAO since last retrieval date)? [yes/no]: ").lower()
+        
+        # check user inputs
+        y_inputs = ["y", "yes", "true"]
+        n_inputs = ["n", "no", "false"]
+        valid_inputs = y_inputs + n_inputs
+        if (major_prompt in valid_inputs) and (new_prompt in valid_inputs):
+            
+            # set major_only param
+            if major_prompt.lower() in y_inputs:
+                major_only = True
+            elif major_prompt.lower() in n_inputs:
+                major_only = False
+            
+            # set new_only param
+            if new_prompt.lower() in y_inputs:
+                new_only = True
+                file_name = input("Enter file name of existing dataset (i.e., 'population_major'): ")
+            elif new_prompt.lower() in n_inputs:
+                new_only = False
+                file_name = "population_"
+        
+            # call scraper pipeline
+            main(data_path, major_only=major_only, new_only=new_only, path=data_path, file_name=file_name)
+            break
+
+        else:
+            print(f"Invalid input. Must enter one of the following: {', '.join(valid_inputs)}.")
     
     # calculate time elapsed
     stop = time.process_time()
