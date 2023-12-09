9e9b33ce20a71a0d8c3fe3f03eb51c20c50922d1
Mark Febrizio <63374265+mfebrizio@users.noreply.github.com>
2023-12-08 19:51:49 -0500

    Update __main__.py

--- data/major_rules/cradb/__main__.py
+++ data/major_rules/cradb/__main__.py
@@ -9,13 +9,39 @@ if __name__ == "__main__":
     start = time.process_time()
 
     p = Path(__file__)
+    root_path = p.parents[1]
     data_path = p.parents[1].joinpath("raw_data")
     if not data_path.is_dir():
         data_path.mkdir(parents=True, exist_ok=True)
 
-    status = scraper(data_path)
-    if status:
-        process_data(data_path)
+    while True:
+        
+        prompt = input("Do you want to retrieve data [r], process data [p], or both [b]? Enter selection [r/p/b]: ").lower()
+
+        retrieve = ("r", "retrieve")
+        process = ("p", "process")
+        both = ("b", "both")
+        valid_inputs = retrieve + process + both
+        
+        if prompt in retrieve:
+            scraper(data_path)
+            break
+        elif prompt in process:
+            process_data(data_path, root_path)
+            break
+        elif prompt in both:        
+            status = scraper(data_path)
+            if status:
+                process_data(data_path, root_path)
+            else:
+                print("""
+                      Can't process data without retrieving rule-level details.
+                      \n
+                      Only rule-level webpages contain data on published dates.
+                      """)
+            break
+        else:
+            print(f"Invalid input. Must enter one of the following: {', '.join(valid_inputs)}.")
 
     # calculate time elapsed
     stop = time.process_time()
