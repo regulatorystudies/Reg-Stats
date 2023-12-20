d1fe9e8c51a8d88dd1bb8edd39a01f8e46841d60
HenryHirsch <122999149+henhirsch@users.noreply.github.com>
2023-12-20 16:50:14 -0500

    deleted line graph theme

--- charts/code/style.R
+++ charts/code/style.R
@@ -34,8 +34,7 @@
   showtext_auto()
     # according to documentation, showtext_auto needed for this to work properly 
   
-# custom chart theme for bar plot ####
-  # Should we change this to "bartheme" or just delete the other theme?
+# custom theme for RSC Reg Stats plots
   theme_RSC <- theme_minimal() +
     theme(
       plot.title = element_text(hjust = 0.5, vjust = 0, margin = margin(b = 10, unit = "pt"), size = 20),
@@ -52,25 +51,6 @@
       plot.margin = margin(50, 50, 50, 50),
       axis.ticks.x = element_line(color = RSCgray)
     )
-  
-# custom chart theme for line plots####
-  
-  # linetheme <- theme_minimal() +
-  #   theme(
-  #     plot.title = element_text(hjust = 0.5, vjust=-2, margin = margin(b = 40, unit = "pt"), size=20),
-  #     axis.text.x = element_text(angle = 65, hjust = 1, vjust = 1,size=12),
-  #     legend.position = "right",
-  #     panel.grid.major.x = element_blank(),
-  #     axis.text.y = element_text(size=12),
-  #     axis.title.y = element_text(size = 12, angle = 90, vjust = 1),
-  #     panel.grid.major.y = element_line(color = RSCgray, linetype = "solid"),
-  #     panel.grid.minor = element_blank(),
-  #     text = element_text(family = "avenir_lt_pro"),
-  #     plot.caption = element_text(hjust = 1, margin = margin(t = 0, l = 6, unit = "pt"),size=11),
-  #     plot.margin = margin(50, 50, 50, 50),
-  #     axis.ticks.x = element_line(color = RSCgray)
-  #   )
-  # ^ delete?
   
   # thank you to Yaxin for developing this custom theme!
   
