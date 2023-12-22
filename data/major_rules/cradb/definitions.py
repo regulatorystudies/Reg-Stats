19b9cd12a5809c8096056c4794d3e21ae72e79d9
Mark Febrizio <63374265+mfebrizio@users.noreply.github.com>
2023-12-22 11:34:10 -0500

    Create definitions.py
    add variables definitions in py file

---
+++ data/major_rules/cradb/definitions.py
@@ -0,0 +1,27 @@
+POPULATION_VARIABLES = {
+    "page": "Page number the rule appeared on in database query. May differ across requests.",
+    "url": "Unique link to the rule's detailed information page on gao.gov.",
+    "type": "Determination of whether a rule is major or non-major as defined by the CRA in 5 U.S.C. 804(2).",
+    "identifier": "Regulation Identification Number (RIN) assigned by OIRA.",
+    "fed_reg_number": "Federal Register citation to the published document.",
+    "effective_date": "Effective date of the rule as specified in the Federal Register.",
+    "received": "Date the rule was received by GAO.",
+    "priority": "Classification that maps loosely to OIRA's RIN priority designation. Values include 'Significant/Substantive' and 'Routine/Info/Other'."
+    }
+
+RULE_VARIABLES = {
+    "url": "Unique link to the rule's detailed information page on gao.gov.", 
+    "title": "Title of the rule.", 
+    "agency": "Top-level agency issuing the rule.", 
+    "subagency": "Subagency issuing the rule.", 
+    "type": "Determination of whether a rule is major or non-major as defined by the CRA in 5 U.S.C. 804(2).", 
+    "description": "Description of the rule's action. Generally very similar to the title.", 
+    "priority": "Classification that maps loosely to OIRA's RIN priority designation. Values include 'Significant/Substantive' and 'Routine/Info/Other'.", 
+    "effective": "Effective date of the rule as specified in the Federal Register.", 
+    "received": "Date the rule was received by GAO.", 
+    "published": "Publication date of the rule in the Federal Register.", 
+    "control_number": "Unique identifier assigned by GAO.", 
+    "fed_reg_number": "Federal Register citation to the published document.", 
+    "identifier": "Regulation Identification Number (RIN) assigned by OIRA.", 
+    "major_rule_report": "Link to the rule's major rule report on gao.gov. May be missing."
+    }
