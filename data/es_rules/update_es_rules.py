d12b62413b877a187be1feb3bad452532309d63a
Zoey X <37126521+zhoudanxie@users.noreply.github.com>
2024-02-26 12:18:55 -0500

    Updating agency ES rules (#38)
    * Updated 2023 data
    
    * Update update_es_rules.py
    
    * Update update_sig_rules.py
    
    * Create update_agency_es_rules.py
    
    * Update update_sig_rules.py
    
    * Update update_es_rules.py
    
    * Updated 2023 data and changed format
    
    * Updated 2023 data and changed format
    
    * Updated 2023 data and changed format
    
    * Updated 2023 data and changed format
    
    * Updated 2023 data and changed format
    
    * Updated 2023 data and changed format
    
    * Updated 2023 data and changed format
    
    * Updated 2023 data and changed format
    
    * Updated 2023 data and changed format
    
    * Updated 2023 data and changed format
    
    * Updated 2023 data and changed format
    
    * Update president_party_year.csv
    
    add data for 2023
    
    * revise datasets
    
    checked manually with fr_tracking csv
    
    * Update update_agency_es_rules.py
    
    remove unused import; adjust regex to account for extra whitespace; broke out find_agency function to be standalone; removed unneeded return
    
    ---------
    
    Co-authored-by: Mark Febrizio <63374265+mfebrizio@users.noreply.github.com>

--- data/es_rules/update_es_rules.py
+++ data/es_rules/update_es_rules.py
@@ -17,13 +17,13 @@ else:
 
 #%% Find years to be updated
 # The latest data-year in the current dataset
-last_year_with_data=df['Presidential Year (February 1 - January 31)'].iloc[-1]
+last_year_with_data=int(df['Presidential Year (February 1 - January 31)'].iloc[-1])
 
 # Years to be updated
 current_year = date.today().year
 
 if last_year_with_data<current_year-1:
-    first_year_to_update=last_year_with_data+1
+    first_year_to_update=max(last_year_with_data+1,2021)
     last_year_to_update=current_year-1
     print(f'Updating data for presidential year {first_year_to_update}-{last_year_to_update}...')
 else:
@@ -37,7 +37,7 @@ df_fr['publication_date'] = pd.to_datetime(df_fr['publication_date'], format="mi
 df_fr['econ_significant'] = pd.to_numeric(df_fr['econ_significant'], errors='coerce')
 df_fr['3(f)(1) significant']=pd.to_numeric(df_fr['3(f)(1) significant'], errors='coerce')
 
-#%% Get user input pn presidential party
+#%% Get user input on presidential party
 def input_party(year):
     party_option = ['democratic','d','republican','r']
     while True:
