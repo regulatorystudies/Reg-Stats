e54ac72db98779ec88ac022d01cf5305cd457a44
mfebrizio <mfebrizio@gwu.edu>
2024-06-04 23:33:45 -0400

    update tests

--- data/major_rules/tests/__main__.py
+++ data/major_rules/tests/__main__.py
@@ -1,8 +1,17 @@
-from .tests import ALL_TESTS
+from .tests import (
+    ALL_TESTS, 
+    cleanup, 
+    scraper_examples, 
+    )
 
 if __name__ == "__main__":
     
     for func in ALL_TESTS:
         func()
     
+    # deletes test files
+    cleanup()
+    
     print("Tests complete.")
+    
+    scraper_examples()
