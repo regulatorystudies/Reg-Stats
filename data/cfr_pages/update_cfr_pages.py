b99b737785eed6f6fab33d627aed10cc871fa4ec
zhoudanxie <zxie@gwu.edu>
2024-07-03 17:23:59 -0400

    Update update_cfr_pages.py

--- data/cfr_pages/update_cfr_pages.py
+++ data/cfr_pages/update_cfr_pages.py
@@ -4,6 +4,34 @@ from PyPDF2 import PdfFileReader
 import pandas as pd
 from datetime import datetime
 import sys
+from tqdm import tqdm
+
+import warnings
+warnings.filterwarnings("ignore")
+
+#%% Import the latest dataset
+dir_path=os.path.dirname(os.path.realpath(__file__))
+df_full=pd.read_excel(f'{dir_path}/aggregated_charts_frstats.xlsx',sheet_name='CFR Vols',skiprows=4)
+#print(df_full.info())
+
+#%% Separate data and notes
+df_data=df_full[df_full['Year'].astype('str').str.isnumeric()]
+df_data['Year']=df_data['Year'].astype('int')
+df_data=df_data[['Year','Total Pages']][df_data['Year']>=1950]
+
+df_notes=df_full[df_full['Year'].astype('str').str.contains(r'\*')][['Year']]
+
+#%% Identify start and end years
+last_year=df_data['Year'].iloc[-1]
+
+start_year=int(last_year)+1
+end_year=datetime.today().year
+
+if end_year>start_year:
+    print(f"Data for {start_year}-{end_year} will be updated. It may take a long time.")
+else:
+    print("The current dataset is up-to-date. No update is needed.")
+    sys.exit(0)
 
 #%% Function to download a file
 def download_file(url,file_path):
@@ -11,7 +39,7 @@ def download_file(url,file_path):
     if r.status_code == 200:
         r = requests.get(url, allow_redirects=True)
         open(file_path, 'wb').write(r.content)
-        print(f"{file_path} has been downloaded.")
+        #print(f"{file_path} has been downloaded.")
         downloaded=True
     else:
         downloaded=False
@@ -21,31 +49,19 @@ def download_file(url,file_path):
 #%% Function to count pages
 def count_pages(file_path):
     with open(file_path, "rb") as f:
-        pdf = PdfFileReader(f)
+        pdf = PdfFileReader(f, strict=False)
         pages=pdf.getNumPages()
     return pages
-
-#%% Identify start and end years
-df_exist=pd.read_csv('data/cfr_pages/cfr_pages_by_calendar_year.csv',skiprows=4)
-last_year=df_exist[df_exist['Year'].astype('str').str.isnumeric()]['Year'].iloc[-1]
-
-start_year=int(last_year)+1
-end_year=datetime.today().year
-
-if end_year>start_year:
-    print(f"Data for {start_year}-{end_year} are being updated...")
-else:
-    print("The current dataset is up-to-date. No update is needed.")
-    sys.exit(0)
 
 #%% Collect data
 pages_list=[]
 for year in range(start_year,end_year):
-    for title in range(1,51):
+    print(f"Page counts for CFR {year} are being collected...")
+    for title in tqdm(range(1,51)):
         vol=1
         while vol<100:      #check all possible volumes
             url = f"https://www.govinfo.gov/content/pkg/CFR-{year}-title{title}-vol{vol}/pdf/CFR-{year}-title{title}-vol{vol}.pdf"
-            file_path = f"data/cfr_pages/temp_files/CFR-{year}-title{title}-vol{vol}.pdf"
+            file_path = f"{dir_path}/CFR-{year}-title{title}-vol{vol}.pdf"
 
             downloaded=download_file(url,file_path)
 
@@ -62,7 +78,7 @@ for year in range(start_year,end_year):
 
     # Index pages
     url=f"https://www.govinfo.gov/content/pkg/GPO-CFR-INDEX-{year}/pdf/GPO-CFR-INDEX-{year}.pdf"
-    file_path=f"data/cfr_pages/temp_files/GPO-CFR-INDEX-{year}.pdf"
+    file_path=f"{dir_path}/GPO-CFR-INDEX-{year}.pdf"
 
     download_file(url,file_path)
 
@@ -72,19 +88,34 @@ for year in range(start_year,end_year):
     else:
         pass
 
-#%% Save disaggregated data
-print(len(pages_list))
-
+#%% Convert results to a dataframe
 df=pd.DataFrame(pages_list,columns=['year','title','vol','pages'])
-print(df.info())
-df.to_csv(f"data/cfr_pages/cfr_pages_disaggregated_{min(df['year'])}_{max(df['year'])}.csv",index=False)
+#print(df.info())
 
-#%% Concatenate new data
+#%% Save disaggregated data
+df_disagg=pd.read_csv(f'{dir_path}/cfr_pages_disaggregated.csv')
+
+df_disagg=pd.concat([df_disagg,df[df['year']>max(df_disagg['year'])]],ignore_index=True)
+df_disagg.to_csv(f"{dir_path}/cfr_pages_disaggregated.csv",index=False)
+
+#%% Concatenate annual data
 df_year=df[['year','pages']].groupby('year').sum().reset_index()
 df_year.rename(columns={'year':'Year','pages':'Total Pages'},inplace=True)
-print(df_year)
+#print(df_year)
 
+df_data=pd.concat([df_data,df_year],ignore_index=True)
 
+#%% Concatenate notes
+# Add data source
+df_notes.loc[-1]=[f"Data source: GovInfo (https://www.govinfo.gov/app/collection/cfr) for years {start_year}-{end_year-1};"
+                  f" Federal Register Statistics (https://www.federalregister.gov/reader-aids/federal-register-statistics)"
+                  f" for all the prior years."]
 
+#%%
+df_data=pd.concat([df_data,df_notes],ignore_index=True)
+
+#%% Export updated annual data
+df_data.to_csv(f'{dir_path}/cfr_pages_by_calendar_year.csv',index=False)
+print("Data have been updated and saved. END.")
 
 
