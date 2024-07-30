7272eaf25072a65c8be08f6934dd1ddd0e2f8144
Mark Febrizio <63374265+mfebrizio@users.noreply.github.com>
2024-07-29 22:02:50 -0400

    Update version.py
    bump version

--- data/fr_tracking/rule_tracking/version.py
+++ data/fr_tracking/rule_tracking/version.py
@@ -1,7 +1,7 @@
 __release__ = {
     "title": "rule_tracking", 
     "description": "Program to collect Federal Register documents for the tracking significant and major rules.", 
-    "version": "0.2.0", 
+    "version": "0.2.1", 
     "author": "Mark Febrizio", 
     "license": "MIT License"
     }
