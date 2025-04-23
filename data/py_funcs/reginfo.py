83ff136d5ac0d2648fe62727300595ef864a2747
Henry Hirsch <122999149+henry-hirsch@users.noreply.github.com>
2025-04-23 15:12:49 -0400

    Update reginfo.py

--- data/py_funcs/reginfo.py
+++ data/py_funcs/reginfo.py
@@ -1,7 +1,14 @@
+#%%
 import pandas as pd
 import urllib.request
 import re
 from dateutil.relativedelta import relativedelta
+#from agencies import parse_reginfo_xml, xml_file_path
+
+#%% test
+# import pprint
+# agency_dict = parse_reginfo_xml(xml_file_path)
+# pprint.pprint(agency_dict)
 
 #%% Function to remove html tags
 CLEANR = re.compile('<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});')
@@ -10,13 +17,13 @@ def cleanhtml(text):
     return cleantext
 
 #%% Function to fetch a count between given dates from reginfo.gov
-def get_reginfo_data(start_date,end_date,rule_type='es'):
-    # date format: MM/DD/YYYY; rule_type='es' or 'sig'
+def get_reginfo_data(start_date,end_date,rule_type='es',agency_code='0000'):
+    # date format: MM/DD/YYYY; rule_type='es' or 'sig'; default agency code for "All Agencies" = 0000
 
     # Reginfo.gov hidden API (obtained through network monitor feature on web browsers)
     es_para='econSigs=Yes&' if rule_type=='es' else ''
 
-    url='https://www.reginfo.gov/public/do/eoAdvancedSearch?rin=&eoStatusCode=CD&agencyCode=0000&subAgencyCode=' \
+    url=f'https://www.reginfo.gov/public/do/eoAdvancedSearch?rin=&eoStatusCode=CD&agencyCode={agency_code}&subAgencyCode=' \
         f'&_s3f1Sigs=on&terms=&{es_para}_econSigs=on&_legalDeadlines=on&receivedStartDate=&receivedEndDate=' \
         '&_ruleStages=on&ruleStages=4&ruleStages=5&ruleStages=3&_healthcareFlag=on&_internationalFlag=on' \
         '&_doddFrankFlag=on&_tcjaFlag=on&_expeditedFlag=on&_covid19Flag=on&concludedActionCode=&conclusionStartDate=' \
@@ -29,7 +36,7 @@ def get_reginfo_data(start_date,end_date,rule_type='es'):
     retry_count = 0
     while retry_count < max_retries:
         try:
-            f = urllib.request.urlopen(url, timeout=15)
+            f = urllib.request.urlopen(url, timeout=10)
             # Rest of your code
             break  # Exit the loop if the request is successful
         except urllib.error.URLError as e:
@@ -89,11 +96,11 @@ def count_reginfo_monthly(update_start_date, update_end_date, rule_type='es'):
     return df_update
 
 # %% Function to collect reginfo data for multiple presidential years
-def count_reginfo_annual(start_year, end_year, rule_type='es'):
+def count_reginfo_annual(start_year, end_year, agency_code, rule_type='es'):
     result_dict = {}
     for year in range(start_year, end_year + 1):
         start_date = f'02/01/{year}'
         end_date = f'01/31/{year + 1}'
-        result_dict[year] = get_reginfo_data(start_date, end_date, rule_type)
+        result_dict[year] = get_reginfo_data(start_date, end_date, rule_type, agency_code)
 
     return result_dict
