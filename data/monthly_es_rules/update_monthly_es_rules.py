4f2a31cb0c818b249bcbd883103e9ec05f0bb70c
Zoey Xie <zxie@gwu.edu>
2025-05-23 14:25:05 -0400

    Revert "remove unnecessary monthly ES rules script and dataset"
    This reverts commit bdb5d011a5c023cfa19c41f1355ff3dac03d364b.

---
+++ data/monthly_es_rules/update_monthly_es_rules.py
@@ -0,0 +1,25 @@
+#----------------------Code to Update Monthly Economically Significant Rules by Presidential Month---------------------
+#-----------------------------------------Author: Zhoudan Xie----------------------------------------------------------
+
+#%% Import packages
+import os
+import sys
+
+# Import customized functions
+dir_path=os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, f'{dir_path}/../py_funcs')
+import update_admin
+
+#%% Specify the file path of the current dataset
+file_path=f'{dir_path}/monthly_econ_significant_rules_by_presidential_month.csv'
+
+#%% Specify rule type and data type
+rule_type='es'
+data_type='monthly'
+
+#%% Run functions to update the data
+df=update_admin.main(dir_path,file_path,rule_type,data_type)
+
+#%% Export
+df.to_csv(file_path, index=False)
+print('The dataset has been updated and saved. End of execution.')
