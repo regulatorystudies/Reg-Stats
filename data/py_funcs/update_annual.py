f3c2527c67e443d46386d6e08a565931faa6905e
Henry Hirsch <122999149+H-Hirsch@users.noreply.github.com>
2025-09-08 16:26:10 -0400

    documentation

--- data/py_funcs/update_annual.py
+++ data/py_funcs/update_annual.py
@@ -12,11 +12,11 @@ def update_data(dir_path,col_update,first_year_to_update,last_year_to_update,rul
     else:    
         print(f"Collecting data from reginfo.gov for presidential years {first_year_to_update}-2020...")
         if agency_acronym=='':
-            agency_code = '0000'
+            agency_code = '0000' # 0000 = agency code for "all agencies", the default code if no agency is specified
         else:
             agency_code=agency_dict[agency_acronym][1]
         new_data_dict=count_reginfo_annual(first_year_to_update,2020,agency_code,rule_type)
-        print(f"Collecting data from FR tracking for presidential years 2021-{last_year_to_update}...")
+        print(f"Collecting data from FR tracking for presidential years 2021-{last_year_to_update}...") # presidential year is the typical term used to refer to one full year of an administration (Feb - Jan)
         new_data_dict= new_data_dict | count_fr_annual(dir_path,2021,last_year_to_update,rule_type,agency_acronym)
 
     # Convert to dataframe
@@ -57,7 +57,7 @@ def verify_previous_data(dir_path,df,col_update,earliest_year,last_year_with_dat
         # Compare with the original data
         print('Comparing newly collected data with original data. Differences (if any) will be shown here.')
         if agency_acronym =='':
-            old_data_original=dict(zip(df['Presidential Year (February 1 - January 31)'],
+            old_data_original=dict(zip(df['Presidential Year (February 1 - January 31)'], # REVISIT lines 59-64 for documentation
                                    df[col_update].fillna(-1).astype('int')))
         else:
             old_data_original=dict(zip(df[df['Agency Acronym']==agency_acronym]['Presidential Year (February 1 - January 31)'],
