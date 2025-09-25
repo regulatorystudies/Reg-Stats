931ffa6501e1d599c4aa72de7f59f1e0ca97eeb4
Finn Dobkin <fdobkin59@email.gwu.edu>
2025-09-25 16:35:18 -0400

    Update update_admin.py

--- data/py_funcs/update_admin.py
+++ data/py_funcs/update_admin.py
@@ -9,6 +9,7 @@ from party import *
 #%% Function to compare two dataframes and report differences
 def compare_df(df_old_data_original,df_old_data_updated):
     # Fill null values for comparison
+    # -1 value is a null value placeholder that allows you to keep integer data types
     df_old_data_original=df_old_data_original.fillna(-1)
     df_old_data_updated=df_old_data_updated.fillna(-1)
 
@@ -43,6 +44,10 @@ def update_admin(dir_path,df,admin,first_month_no_data,update_start_date,update_
     # Append new data to the current dataset (cumulative or monthly)
     # Append according to data type
     if data_type=='cumulative':
+    '''
+    If there isn't previous monthly data to append the data to, it will start collecting at the beginning month. 
+    If it isn't the first month of the data, it starts appending at the last non-null value. 
+    '''
         cum_count=0 if first_month_no_data==0 else df[df[admin].notnull()][admin].iloc[-1]
         for x in df_update[f'{rule_type}_count']:
             cum_count = cum_count + x
